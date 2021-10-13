2a1619cc22e5e9ed37226c5da66d3deb5213e559
AlfredVetX <85992644+AlfredVetX@users.noreply.github.com>
2021-10-13 15:22:49 +0100

    Add files via upload

---
+++ EOLC Submission.js
@@ -0,0 +1,13 @@
+<!-- Global site tag (gtag.js) - Google Ads: 881259704 -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=AW-881259704"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'AW-881259704');
+</script>
+<!-- Event snippet for Submit lead form conversion page -->
+<script>
+  gtag('event', 'conversion', {'send_to': 'AW-881259704/cQvvCKSm28MCELjpm6QD'});
+</script>
